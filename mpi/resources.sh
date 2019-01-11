136267fbd2ef33e3b53cda627110c7eef85f3a6a
Fabio Rocha Jimenez Vieira <rocha@biologie.ens.fr>
2019-01-11 13:55:24 +0100

    Delete resources.sh

--- mpi/resources.sh
+++
@@ -1,2 +0,0 @@
-qstat | grep " R " | cut -f 1 -d " " | while read jobID; do resource=`qstat -f $jobID | grep "Resource_List.walltime\|Resource_List.ncpus\|Job_Owner" | sed "s/^[ ]\+//" | tr '\n' ' '`; echo "$resource $jobID" ; done
-
